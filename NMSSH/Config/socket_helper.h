6a9e42333549e8cecd7a9d6fec3e2926acd55df0
Sebastian Husche <sebastian@scanbot.io>
2017-12-19 12:48:32 +0100

    Fix warning.

--- NMSSH/Config/socket_helper.h
+++ NMSSH/Config/socket_helper.h
@@ -1,3 +1,4 @@
 #import "libssh2.h"
 
 int waitsocket(int socket_fd, LIBSSH2_SESSION *session);
+int select(int, fd_set * __restrict, fd_set * __restrict, fd_set * __restrict, struct timeval * __restrict);
